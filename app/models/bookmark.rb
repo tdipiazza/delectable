4519aa861280c0214addd346744a5c205d5f7402
Tricia DiPiazza <Treecia@gmail.com>
2011-09-23 14:09:24 -0500

    added validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -11,4 +11,9 @@
 #
 
 class Bookmark < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_presence_of :url
+  validates_length_of :name, :maximum=>100
+  validates_length_of :url, :minimum=>10
 end
